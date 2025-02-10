d4924ff3a650b775d02f7b28ace71ebb6353560c
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-10 23:12:30 +0200

    Revert "fix push"
    This reverts commit e5f7db7dcdc864d1535188dda3452eac04ca5fb8.

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = '961b8d87-f1a5-4fa3-a1d5-b4fb9d66f024'; // ключ OneSignal
   static appID = '6741569054'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
-  static targetDate = '2025-02-13'; // дата закінчення відльожки
+  static targetDate = '2025-02-10'; // дата закінчення відльожки
   static bodyLin = 'https://super-magnificent-elation.space/b8JH1qYB'; // тіло лінки з якою будемо працювати
 }
